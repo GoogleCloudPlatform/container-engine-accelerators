0d1680ff1d92f45db0bcbf4718adb51fa231659e
Pradeep Venkatachalam <pradvenkat@gmail.com>
2020-10-19 08:01:14 -0700

    [Commit 2] Add GPU metric definitions and create prometheus endpoint to expose these metrics.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/prometheus/client_golang v1.8.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20200707034311-ab3426394381
 	google.golang.org/grpc v1.27.1
--- pkg/gpu/nvidia/metrics/metrics.go
+++ pkg/gpu/nvidia/metrics/metrics.go
@@ -1,5 +1,49 @@
 package metrics
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	// DutyCycle reports the percent of time when the GPU was actively processing.
+	DutyCycle = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "duty_cycle",
+			Help: "Percent of time when the GPU was actively processing",
+		},
+		[]string{"namespace", "pod", "container", "make", "accelerator_id", "model"})
+
+	// MemoryTotal reports the total memory available on the GPU.
+	MemoryTotal = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "memory_total",
+			Help: "Total memory available on the GPU in bytes",
+		},
+		[]string{"namespace", "pod", "container", "make", "accelerator_id", "model"})
+
+	// MemoryUsed reports GPU memory allocated.
+	MemoryUsed = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "memory_used",
+			Help: "Allocated GPU memory in bytes",
+		},
+		[]string{"namespace", "pod", "container", "make", "accelerator_id", "model"})
+
+	// AcceleratorRequests reports the number of GPU devices requested by the container.
+	AcceleratorRequests = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "accelerator_requests",
+			Help: "Number of accelerator devices requested by the container",
+		},
+		[]string{"resource_name"})
+)
+
 // MetricServer exposes GPU metrics for all containers in prometheus format on the specified port.
 type MetricServer struct {
 	collectionInterval  int
@@ -17,6 +61,14 @@ func NewMetricServer(collectionInterval, port int, metricsEndpointPath string) *
 
 // Start performs necessary initializations and starts the metric server.
 func (m *MetricServer) Start() {
+	glog.Infoln("Starting metrics server")
+	go func() {
+		http.Handle(m.metricsEndpointPath, promhttp.Handler())
+		err := http.ListenAndServe(fmt.Sprintf(":%d", m.port), nil)
+		if err != nil {
+			glog.Infof("Failed to start metric server: %v", err)
+		}
+	}()
 }
 
 // Stop performs cleanup operations and stops the metric server.
