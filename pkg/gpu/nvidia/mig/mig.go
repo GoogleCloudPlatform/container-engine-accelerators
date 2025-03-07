f7a60a6c72f2568cd1cdef4360f79de9c1e95c21
John Paul Bunn <johnpaulbunn@google.com>
2025-03-07 08:36:40 +0000

    +format

--- pkg/gpu/nvidia/mig/mig.go
+++ pkg/gpu/nvidia/mig/mig.go
@@ -51,11 +51,11 @@ var gpuPartitionSizeMaxCount = map[string]int{
 	"4g.71gb":  1,
 	"7g.141gb": 1,
 	//nvidia-b200
-	"1g.23gb": 7,
-	"1g.45gb": 4,
-	"2g.45gb": 3,
-	"3g.90gb": 2,
-	"4g.90gb": 1,
+	"1g.23gb":  7,
+	"1g.45gb":  4,
+	"2g.45gb":  3,
+	"3g.90gb":  2,
+	"4g.90gb":  1,
 	"7g.180gb": 1,
 }
 
